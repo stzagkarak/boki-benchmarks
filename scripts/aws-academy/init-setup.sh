bfe88b5447dff9e90e10aae8767fc32539f55141
stzagkarak <stzagkarak@gmail.com>
2024-01-06 15:04:01 +0200

    changes

--- scripts/aws-academy/init-setup.sh
+++ scripts/aws-academy/init-setup.sh
@@ -6,11 +6,11 @@
 AUTH='echo "ubuntu"'
 
 # copy built boki executables in machine
-ssh -q ubuntu@$1 -- $AUTH | sudo -S mkdir /boki
-ssh -q ubuntu@$1 -- $AUTH | sudo -S chmod a+rwx /boki
+ssh -q ubuntu@$1 -- "$AUTH | sudo -S mkdir /boki"
+ssh -q ubuntu@$1 -- "$AUTH | sudo -S chmod a+rwx /boki"
 scp -q /home/ec2-user/ubuntu-build/* ubuntu@$1:/boki
 
 # copy a setup script in the machine
 scp -q ./setup.sh ubuntu@$1:~
-ssh -q ubuntu@$1 -- $AUTH | sudo -S chmod a+x ~/setup.sh
-ssh -q ubuntu@$1 -- $AUTH | sudo -S ~/setup.sh $2
+ssh -q ubuntu@$1 -- "$AUTH | sudo -S chmod a+x ~/setup.sh"
+ssh -q ubuntu@$1 -- "$AUTH | sudo -S ~/setup.sh $2"
