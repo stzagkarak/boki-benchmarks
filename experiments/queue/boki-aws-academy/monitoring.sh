7734da774ecf9491c7c499a54ffcfcb4bdfb064b
stzagkarak <stzagkarak@gmail.com>
2024-01-11 18:01:49 +0200

    changes

--- experiments/queue/boki-aws-academy/monitoring.sh
+++ experiments/queue/boki-aws-academy/monitoring.sh
@@ -4,14 +4,20 @@ if [ ! -d ~/monitoring ]
   then mkdir ~/monitoring
 fi
 
-cdate=$(date '+%H-%M-%S-%N')
+rm -rf ~/monitoring/$1
 mkdir -p ~/monitoring/$1
 
 freefd=~/monitoring/$1/free.out
 iostatfd=~/monitoring/$1/iostat.out
+mpstatfd=~/monitoring/$1/mpstat.out
+pidstatfd=~/monitoring/$1/pidstat.out
+cifsiostatfd=~/monitoring/$1/cifsiostat.out
 
 touch $freefd
 touch $iostatfd
+touch $mpstatfd
+touch $pidstatfd
+touch $cifsiostatfd
 
 sleep 10
 
@@ -27,6 +33,18 @@ do
     # iostat
     echo "-- LOOP $i | $dt" >> $iostatfd
     iostat -h >> $iostatfd
+
+    # mpstat 
+    echo "-- LOOP $i | $dt" >> $mpstatfd
+    mpstat -h >> $mpstatfd
+
+    # pidstat
+    echo "-- LOOP $i | $dt" >> $pidstatfd
+    pidstat -h >> $pidstatfd
+
+    # cifsiostat
+    echo "-- LOOP $i | $dt" >> $cifsiostatfd
+    cifsiostat -h >> $cifsiostatfd
 
     sleep 10
 done
