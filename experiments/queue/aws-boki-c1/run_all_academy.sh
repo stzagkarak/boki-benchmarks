04eb96f7b8b2489e87d9edf44c7a4552448389f3
stzagkarak <stzagkarak@gmail.com>
2024-01-16 12:29:47 +0200

    changes

--- experiments/queue/aws-boki-c1/run_all_academy.sh
+++ experiments/queue/aws-boki-c1/run_all_academy.sh
@@ -13,10 +13,10 @@ echo "Running run_once_academy.sh p32c128 128 3 1 32"
 $BASE_DIR/run_once_academy.sh p32c128 128 3 1 32
 
 echo "Running run_once_academy.sh p128c32 32 8  1 128"
-#$BASE_DIR/run_once_academy.sh p128c32 32 8  1 128
+$BASE_DIR/run_once_academy.sh p128c32 32 8  1 128
 
 echo "Running run_once_academy.sh p128c128 128 6 1 128"
-#$BASE_DIR/run_once_academy.sh p128c128 128 6 1 128
+$BASE_DIR/run_once_academy.sh p128c128 128 6 1 128
 
 # clean the machines up
 echo "Running disband-machines on ACADEMY_HELPER_SCRIPT"
--- experiments/queue/aws-boki-c1/run_once_academy.sh
+++ experiments/queue/aws-boki-c1/run_once_academy.sh
@@ -72,11 +72,11 @@ ssh -q $MANAGER_HOST -- docker stack deploy \
 echo "Sleeping for 60 secs"
 sleep 60
 
-echo "Optimizing docker on ALL_ENGINE_HOSTS"
-for HOST in $ALL_ENGINE_HOSTS; do
-    ENGINE_CONTAINER_ID=`$ACADEMY_HELPER_SCRIPT get-container-id --base-dir=$BASE_DIR --service faas-engine --machine-host $HOST`
-    echo 2048 | ssh -q $HOST -- sudo tee /sys/fs/cgroup/cpu,cpuacct/docker/$ENGINE_CONTAINER_ID/cpu.shares
-done
+#echo "Optimizing docker on ALL_ENGINE_HOSTS"
+#for HOST in $ALL_ENGINE_HOSTS; do
+#    ENGINE_CONTAINER_ID=`$ACADEMY_HELPER_SCRIPT get-container-id --base-dir=$BASE_DIR --service faas-engine --machine-host $HOST`
+#    echo 2048 | ssh -q $HOST -- sudo tee /sys/fs/cgroup/cpu,cpuacct/docker/$ENGINE_CONTAINER_ID/cpu.shares
+#done
 
 QUEUE_PREFIX=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
